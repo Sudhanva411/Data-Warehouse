156defec542d23656ed2a37bce52836bb74038ab
Sudhanva411 <64422146+Sudhanva411@users.noreply.github.com>
2026-09-09 14:39:45 +0530

    Enhance load_tables.sql for multiple table handling
    Added truncation and loading commands for multiple tables and updated the row count selection query.

--- scripts/load_tables.sql
+++ scripts/load_tables.sql
@@ -1,7 +1,18 @@
 BEGIN;
 
-TRUNCATE TABLE bronze.crm_cust_info;\copy bronze.crm_cust_info 
-FROM 'C:/Users/sasudhan/Downloads/datasets/source_crm/cust_info.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
+TRUNCATE TABLE bronze.crm_cust_info;
+TRUNCATE TABLE bronze.crm_prd_info;
+TRUNCATE TABLE bronze.crm_sales_details;
+
+-- Have to run the below copy commands line by line as \copy is terminal cmd and requires physical line breaks
+\copy bronze.crm_cust_info FROM 'C:/Users/sasudhan/Downloads/datasets/source_crm/cust_info.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',')
+\copy bronze.crm_prd_info FROM 'C:/Users/sasudhan/Downloads/datasets/source_crm/prd_info.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',')
+\copy bronze.crm_sales_details FROM 'C:/Users/sasudhan/Downloads/datasets/source_crm/sales_details.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',')
+
 COMMIT;
 
-SELECT COUNT(*) FROM bronze.crm_cust_inf
+SELECT (
+  SELECT COUNT(*) FROM bronze.crm_cust_info) AS cust_rows, 
+  (SELECT COUNT(*) FROM bronze.crm_prd_info) AS prd_rows, 
+  (SELECT COUNT(*) FROM bronze.crm_sales_details) AS sales_rows;
+
